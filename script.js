812b5add2eb8a66570958973da94a86c4aa13f64
Sravya-2021 <152886681+Sravya-2021@users.noreply.github.com>
2023-12-17 11:48:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const header = document("header");
+
+window.addEventListener("scroll", function(){
+    header.classList.toggle("sticky",this.window.scrollY > 0);
+});
+
+let menu=document.querySelector('#menu-icon');
+let nav=document.querySelector('.nav');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    nav.classList.toggle('open');
+}
